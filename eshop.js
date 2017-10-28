b60dc5822160dfe7f22dfdddfa85feb8a548bad7
Ryasik Oksana <ryasik.oksana@yandex.ru>
2017-10-28 23:06:22 +0700

    Add files via upload

---
+++ eshop.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+  loadGoods ();
+});
+
+function loadGoods() {
+  // загружаем товары на сттраницу
+  $getGSON("goods.json", function(data) {
+  //console.log(data);   
+    var out = "";
+    for (var key in data){
+    out+="<p>"+data[key]["name"]+"</p>";
+    out+="<p>"+data[key]["cost"]+"</p>";
+    }
+    $(".goods").html(out);
+  })
+}
